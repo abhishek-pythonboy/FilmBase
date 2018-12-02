107877252aa0874a53da5943e0bce04e8b09ecf6
Abhishek Biswas <abhishek.blogger05@gmail.com>
2018-12-02 09:56:39 +0530

    filmbase 1.0

--- js/main.js
+++ js/main.js
@@ -1,96 +1,95 @@
 const apiKey = "http://www.omdbapi.com/?&apikey=988358d3";
+
 function onLoad() {
-  // change max value of year input to current year
-  let d = new Date();
-  let year = d.getFullYear();
-  document.getElementById("input-year").max = year;
+    // change max value of year input to current year
+    let d = new Date();
+    let year = d.getFullYear();
+    document.getElementById("input-year").max = year;
 }
 // i=tt3896198 removed out of apiKey
 let clickCount = 0;
 // set a listener on submit
-  document.getElementById("submit").addEventListener("click", (e) => {
+document.getElementById("submit").addEventListener("click", (e) => {
 
-  clickCount++;
-  // if user searches for more than once, unslick the previous results
-  if (clickCount > 1) {
-    $("#show-results").slick("unslick");
-  }
-
-  let inputName = document.getElementById("input-name").value;
-  let inputYear = document.getElementById("input-year").value;
-
-  let showResults = document.getElementById("show-results");
-  // empty div
-  showResults.innerHTML = "";
-  // prepare request url
-  let requestUrl = apiKey;
-  if(inputName !== "") {
-    requestUrl += "&s=" + inputName.replace(" ", "+");
-    if (inputYear !== "") {
-      requestUrl += "&y=" + inputYear;
-    }
-  }
-  // make a request
-  let request = new XMLHttpRequest();
-
-  request.open('GET', requestUrl, true);
-  request.onload = () => {
-    // get response and JSONify it
-    let responseData = JSON.parse(request.response);
-
-
-    let i;
-    for (i=0;i<responseData.Search.length;i++) {
-      let poster = (responseData.Search[i].Poster == "N/A") ? "images/default-thumb.jpg" : responseData.Search[i].Poster;
-      showResults.innerHTML += 
-                                "<a href='movie-info.html' onclick=setSession('" + responseData.Search[i].imdbID + "') data-lity>" +
-                                "<div class='result'>"+
-                                "<img src="+poster+" alt='' class='thumbnail'>" +
-                                "<div class='info'>" +
-                                "<h2 class='movie-name'>"+responseData.Search[i].Title+"</h2>" +
-                                "<span class='year'>&nbsp;("+responseData.Search[i].Year+")</span>"
-                                "</div></div></a>";
-                                //console.log(apiKey+"&i="+responseData.Search[i].imdbID);
+    clickCount++;
+    // if user searches for more than once, unslick the previous results
+    if (clickCount > 1) {
+        $("#show-results").slick("unslick");
     }
 
-    $("#show-results").slick({
-      infinite:true,
-      slidesToShow: 6,
-      responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow:5
+    let inputName = document.getElementById("input-name").value;
+    let inputYear = document.getElementById("input-year").value;
+    let showResults = document.getElementById("show-results");
+    // empty div
+    showResults.innerHTML = "";
+    // prepare request url
+    let requestUrl = apiKey;
+    if (inputName !== "") {
+        requestUrl += "&s=" + inputName.replace(" ", "+");
+        if (inputYear !== "") {
+            requestUrl += "&y=" + inputYear;
         }
-      },
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow:4
-        }
-      },
-      {
-        breakpoint: 850,
-        settings: {
-          slidesToShow:3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow:2
-        }
-      }
-      ]
-    });
-  }
+    }
+    // make a request
+    let request = new XMLHttpRequest();
 
-  request.send();
-e.preventDefault();
+    request.open('GET', requestUrl, true);
+    request.onload = () => {
+        // get response and JSONify it
+        let responseData = JSON.parse(request.response);
+
+
+        let i;
+        for (i = 0; i < responseData.Search.length; i++) {
+            let poster = (responseData.Search[i].Poster == "N/A") ? "images/default-thumb.jpg" : responseData.Search[i].Poster;
+            showResults.innerHTML +=
+                "<a href='movie-info.html' onclick=setSession('" + responseData.Search[i].imdbID + "') data-lity>" +
+                "<div class='result'>" +
+                "<img src=" + poster + " alt='' class='thumbnail'>" +
+                "<div class='info'>" +
+                "<h2 class='movie-name'>" + responseData.Search[i].Title + "</h2>" +
+                "<span class='year'>&nbsp;(" + responseData.Search[i].Year + ")</span>"
+            "</div></div></a>";
+        }
+
+        // responsive settings
+        $("#show-results").slick({
+            infinite: true,
+            slidesToShow: 6,
+            responsive: [{
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 5
+                    }
+                },
+                {
+                    breakpoint: 1000,
+                    settings: {
+                        slidesToShow: 4
+                    }
+                },
+                {
+                    breakpoint: 850,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                }
+            ]
+        });
+    }
+
+    request.send();
+    e.preventDefault();
 
 });
 
-  
+// set sessionStorage for movie-info.html
 function setSession(mvID) {
-  sessionStorage.setItem("mvID", mvID);
+    sessionStorage.setItem("mvID", mvID);
 }
--- js/movie-info.js
+++ js/movie-info.js
@@ -2,53 +2,48 @@ window.onload = showMovieInfo;
 
 function showMovieInfo() {
 
-  console.log('yo');
 
-const apiKey = "http://www.omdbapi.com/?&apikey=988358d3";
-let mvID = sessionStorage.getItem("mvID");
+    const apiKey = "http://www.omdbapi.com/?&apikey=988358d3";
+    let mvID = sessionStorage.getItem("mvID");
 
-      let requestUrl = apiKey + "&i=" + mvID;
+    // send request and recieve response
+    let requestUrl = apiKey + "&i=" + mvID;
     let request = new XMLHttpRequest();
-    console.log(requestUrl);
-    
 
     request.open('GET', requestUrl, true);
     request.onload = () => {
-      let responseData = JSON.parse(request.response);
-      console.log(responseData);
+        let responseData = JSON.parse(request.response);
 
-      let posterSrc = (responseData.Poster == "N/A") ? "images/default-thumb.jpg" : responseData.Poster;
-      $("#mv-poster").attr("src", posterSrc);
+        let posterSrc = (responseData.Poster == "N/A") ? "images/default-thumb.jpg" : responseData.Poster;
+        $("#mv-poster").attr("src", posterSrc);
 
-      $("#iFrame body").css("background-color", "transparent");
-      $("#mv-bg").css({
-        "background-image": "url(" + posterSrc + ")",
-        "background-size": "cover"
-      });
+        // 
+        $("#mv-bg").css({
+            "background-image": "url(" + posterSrc + ")",
+            "background-size": "cover"
+        });
 
-      $("#mv-name").html(responseData.Title);
-      $("#mv-year").html("("+responseData.Year+")");
-      $("#rating-num").html(responseData.imdbRating);
-      $("#mv-plot").html(responseData.Plot);
-      $("#mv-director").html(responseData.Director);
-      $("#mv-writer").html(responseData.Writer);
-      $("#mv-stars").html(responseData.Actors);
-      $("#mv-genre").html(responseData.Genre);
-      $("#mv-awards").html(responseData.Awards);
-      $("#mv-production").html(responseData.Production);
-      $("#mv-website").attr("href", responseData.Website);
-      $("#mv-website").html(responseData.Website);
-      $("#mv-rated").html(responseData.Rated);
-      $("#mv-released").html(responseData.Released);
-      $("#mv-runtime").html(responseData.Runtime);
-      $("#mv-boxoffice").html(responseData.BoxOffice);
+        $("#mv-name").html(responseData.Title);
+        $("#mv-year").html("(" + responseData.Year + ")");
+        $("#rating-num").html(responseData.imdbRating);
+        $("#mv-plot").html(responseData.Plot);
+        $("#mv-director").html(responseData.Director);
+        $("#mv-writer").html(responseData.Writer);
+        $("#mv-stars").html(responseData.Actors);
+        $("#mv-genre").html(responseData.Genre);
+        $("#mv-awards").html(responseData.Awards);
+        $("#mv-production").html(responseData.Production);
+        $("#mv-website").attr("href", responseData.Website);
+        $("#mv-website").html(responseData.Website);
+        $("#mv-rated").html(responseData.Rated);
+        $("#mv-released").html(responseData.Released);
+        $("#mv-runtime").html(responseData.Runtime);
+        $("#mv-boxoffice").html(responseData.BoxOffice);
     }
 
     request.send();
     sessionStorage.clear();
 
 
-  event.preventDefault();
-  /* Act on the event */
-}; 
 
+};
